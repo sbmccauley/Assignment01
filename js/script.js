079992061923f288338311e811c63122f011817a
sbmccauley <60712389+sbmccauley@users.noreply.github.com>
2022-07-06 15:43:36 -0700

    assignment 1

---
+++ js/script.js
@@ -0,0 +1,164 @@
+//STEP 1
+// let someMonth;
+// function theMonth();
+// let currentMonth;
+
+//STEP 2
+//Numeric Literal Expression:  3.14
+//String Literal Expression:  'Hello World!'
+//Boolean Literal Expression:  false
+//Null Literal Expression:  null
+
+//STEP 3
+// let complexExample1 = 5 + (6*3)/9;
+// let complexExample2 = 345 - (234
+// /4) + (5*7);
+
+//STEP 4
+// let strFirstName;
+// let strLastName;
+// let strAddress;
+// let strCity;
+// let strState;
+// let numZipCo'de;
+// let numYourAge;
+// let strReferralSource;
+// let strMayWeContactYou;
+
+
+//STEP 5
+
+//3 ways to declare and assign variables:
+//1.
+// let strFirstName = 'Sheri', strLastName = 'McCauley', strAddress = '97 Gates Road';
+
+//2.
+// let strFirstName;
+// let strLastName;
+// let strAddress;
+// strFirstName = 'Sheri';
+// strLastName = 'McCauley';
+// strAddress = '97 Gates Road';
+
+//3.
+// let strFirstName = 'Sheri';
+// let strLastName = 'McCauley';
+// let strAddress = '97 Gates Road';
+
+
+//STEP 6
+// myCoffee = 2
+
+// console.log(`I like to drink ${myCoffee} cups of coffee a day.`);
+
+//STEP 7
+// firstVar = true + 'Hi';
+// console.log(firstVar);
+
+// secondVar = 5 + true;
+// console.log(secondVar);
+
+//STEP 8
+
+//The following string literal is not valid.  To correct and allow it to display as it is intended, the apostrophe in I'm must be escaped using the backslash. I have the corrected version following the original.
+
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former."';
+// window.console.log(someString);
+
+//CORRECTED VERSION
+// let someString = 'Who once said, "Only two things are infinite, the universe and   human stupidity, and I\'m not sure about the former."';
+// window.console.log(someString);
+
+//STEP 9
+// let nullVar = null;
+// let notDef;
+
+// console.log(nullVar);
+// console.log(notDef);
+
+//STEP 10
+// let greeting = 'Good Morning!';
+// let morning = true;
+// let coffee = 2;
+// let rain;
+
+// console.log(typeof greeting);
+// console.log(typeof morning);
+// console.log(typeof coffee);
+// console.log(typeof rain);
+
+//STEP 11
+// alert('Hello Zak Ruvacalba, welcome to the JavaScript class!');
+// alert('Hello ' + 'Sheri McCauley' + ', welcome to the JavaScript class!');
+
+
+//STEP 12
+// let fullName = 'Sheri McCauley';
+// alert('Hello ' + fullName + ', welcome to the JavaScript class!');
+
+
+//STEP 13
+// let fullName = 'Sheri McCauley';
+
+// let course = 'JavaScript';
+// alert(`Hello ${fullName}, welcome to the ${course} class!`);
+
+//STEP 14
+// let fullName = 'Zak Ruvacalba';
+
+// let course = 'JavaScript';
+// alert(`Hello ${fullName}. \n Welcome to the ${course} class!`);
+
+//STEP 15
+// let fullName = prompt('Please enter your name');
+
+// let course = 'JavaScript';
+// alert(`Hello ${fullName}. \n Welcome to the ${course} class!`);
+
+//STEP 16
+// let fullName = prompt('Please enter your name');
+
+// let course = prompt('What course will you be taking?');
+// alert(`Hello ${fullName}. \n Welcome to the ${course} class!`);
+
+//STEP 17
+// let x = 10;
+// let y = 20;
+
+// console.log(x + y);
+
+//STEP 18
+// let x = 20;
+// let y = x+20;
+// console.log(y);
+
+//STEP 19
+// let x = 20;
+// let y = x*5;
+// console.log(y);
+
+//STEP 20
+// let x = 20%3;
+// let y = x/1;
+// console.log(y);
+
+//STEP 21
+// let x = 6;
+// let y = 4+2;
+// console.log (x===y);
+
+//STEP 22
+// let x = 3;
+// let y = 3;
+// console.log(x!=y);  
+
+
+
+
+
+
+
+
+
+
+
